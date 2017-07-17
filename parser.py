685106a7676adabd93c21b5563ecd934249e34c3
ianhhhhhhhhe <517382432@qq.com>
2017-07-17 15:56:53 +0800

    Update

--- parser.py
+++ parser.py
@@ -18,9 +18,9 @@ def parser(url, site_name):
     return res
 
 def mark(tag):
-    if tag == '比特币'.encode('utf-8') or tag == 'Bitcoin':
+    if tag == '比特币' or tag == 'Bitcoin':
         tag = 'bitcoin'
-    if tag == '区块链'.encode('utf-8'):
+    if tag == '区块链':
         tag = 'blockchain'
     tag = 'digitalcoin'
     return tag
